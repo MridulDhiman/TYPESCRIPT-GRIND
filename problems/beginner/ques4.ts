0e4bbe946c6d47cec9d33e22b327fb810af5627e
Mridul Dhiman <dhimanmridul91@gmail.com>
2023-09-09 03:47:24 +0530

    ques4 complete

--- problems/beginner/ques4.ts
+++ problems/beginner/ques4.ts
@@ -28,8 +28,85 @@ const rl = readline.createInterface({
   }); 
 
 
-  const ans = await rl.question("Input a number: ");
-  console.log(ans);
+
+
   
+  
+// array of objects
+let todoList: listItem[] = [
+  {data:"Learn Typescript", id :0}, 
+  {data: "Making Todo List in TS", id: 1},
+];
+
+
+// view list  => make function type 
+type viewListSign = (list: listItem[]) => void;
+type addListSign = (list: listItem[], item: string) => void;
+type deleteListSign = (list: listItem[], id: number) => void;
+
+
+const viewList : viewListSign = (list) => {
+  list.forEach((item) => {
+    console.log(item.data, item.id);
+  })
+}
+
+
+const addToList : addListSign = (list, str) => {
+  const obj: listItem = {
+    data: str,
+    id: list.length,
+  }
+
+  list.push(obj);
+}
+
+const removeFromList : deleteListSign = (list, id) => {
+  if(list.length<0) {
+    console.log("No element found in the list, Enter some value first....");
+    return;
+  }
+  const ind = list.findIndex((el) => el.id === id);
+  if(ind!= -1) {
+    todoList = list.filter((el)=> el.id !== id);
+  }
+  else {
+         console.log(`ID ${id} not found. Enter valid id.....`);
+  }
+}
+
+
+
+
+
+
+  while(true) {
+    const ans = await rl.question(`Input a number: 
+                              1. to view list
+                              2. add data to list
+                              3. remove data from list
+                              4. Exit
+                              Choose an option: `);
+    console.log(ans);
+    if(Number(ans) === 1) {
+      viewList(todoList);
+    }
+    if(Number(ans) === 2) {
+      const enteredData = await rl.question("Enter data: ");
+      addToList(todoList, enteredData);
+     
+    }
+   if(Number(ans) === 3) {
+    const id = await rl.question("Enter id: ");
+    removeFromList(todoList, Number(id));
+   }
+    if(Number(ans) === 4) {
+      break;
+    }
+  }
+  
+  rl.close();
+
+
 
    
